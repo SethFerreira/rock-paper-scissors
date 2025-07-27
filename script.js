6217ab8bc8accad03248ca208acb76cffd528ff2
Seth Ferreira <semfware@gmail.com>
2025-07-27 13:57:18 -0400

    Implement winner determination with cheat code and game play function

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
+// Returns valid user choice or logs error if invalid
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
         return userInput;
     }
     console.log('error');
 }
 
+// Randomly returns 'rock', 'paper', or 'scissors' for the computer's move
 const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
@@ -19,3 +20,41 @@ const getComputerChoice = () => {
     return randomNumber;
 }
 
+// Compares user and computer choices to decide the winner; includes a 'bomb' cheat code
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === 'bomb') {
+        return 'user won!'
+    }
+    if (userChoice === computerChoice) {
+        return 'tie';
+    }
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'computer won';
+        }
+        return 'user won';
+    }
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'computer won';
+        }
+        return 'user won';
+    }
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'computer won';
+        }
+        return 'user won';
+    }
+}
+
+// Starts the game; userChoice must be 'rock', 'paper', 'scissors', or 'bomb'
+const playGame = () => {
+    const userChoice = getUserChoice('scissors');
+    const computerChoice = getComputerChoice();
+    console.log(userChoice);
+    console.log(computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
